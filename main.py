dee617556796900c9961a6c570349621ca91ac55
miguel-rguez <miguel.rodriguez.novoa@udc.es>
2024-05-09 11:49:04 +0200

    añadidas funciones 2,  5, update de la 1, falta corregir una cosa de la 2, y añadir codigos de erres

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import psycopg2.errorcodes
 import psycopg2.extras;
 
 # Parámetros de conexión
-dbname = ''
+dbname = 'bda'
 user = ''
 password = ''
 host = ''  
@@ -22,7 +22,8 @@ host = ''
 def connect_db():
 
     try:
-        conn = psycopg2.connect(dbname=dbname, user=user, password=password)
+        #conn = psycopg2.connect(dbname=dbname, user=user, password=password)
+        conn = psycopg2.connect(dbname=dbname)
 
         conn.autocommit = False
         
@@ -66,18 +67,88 @@ def add_product(conn):
 
     sql="""
             INSERT INTO Producto(nombre,numero_referencia,coleccion,es_personalizable,
-            id_categoria) values(%(n)s, %(r)s, %(c)s, %(p)s, %(i)s)
+            id_categoria) values(%(n)s, %(r)s, %(c)s, %(p)s, %(i)s) returning id
         """
     with conn.cursor() as cur:
         try:
             cur.execute(sql, {'n':name, 'r':n_reference, 'c': colection, 'p':isPersonalizable,
                               'i':category_id})  
             conn.commit()
-            print("Producto Añadido.")
+            id = cur.fetchone()[0]
+            print("Producto Añadido con id: {id}.")
         except psycopg2.Error as e:
-            if e.pgcode == psycopg2.errorcodes.UNIQUE_VIOLATION:
-                print(f"El Producto con referencia{n_reference} ya existe. No se añade")
+            print(f"Error al añadir el producto: {e}")
             conn.rollback()
+##-------------------------------------------------------------
+def add_color(conn):
+    """
+    Añade un nuevo color a la base de datos y muestra el ID del color añadido.
+    """
+    s_product_id = input("Id del Producto que se quiere añadir un nuevo color: ")
+
+    if s_product_id is "":
+        print("Debes introducit un id de producto")
+        return
+    product_id = int(s_product_id)
+    
+    color_name = input("Color del Producto: ")
+    if color_name is "":
+        print("Debes introducir un color de producto")
+        return
+    
+    sprice = input("Precio del producto")
+    price = float(sprice)
+    
+    composition = input("Composición: ")
+    if composition is "":
+        print("Debes introducir una composicion")
+        return
+    
+    cursor = conn.cursor()
+
+    sql="""
+            "INSERT INTO Color(nombre, precio, composicion, id_producto) 
+            VALUES (%(n)s, %(p)s, %(c)s, %(i)s) RETURNING id"
+        """
+    
+    try:
+        # Inserta el color en la tabla y retorna el ID autogenerado
+        cursor.execute(sql, {'n': color_name, 'p': price, 'c': composition, 'i': product_id})
+        # Recupera el ID del color recién insertado
+        color_id = cursor.fetchone()[0]
+        conn.commit()  # Confirma la transacción
+        print(f"Color añadido con éxito. ID: {color_id}")
+    except psycopg2.Error as e:
+        # Maneja cualquier error que ocurra durante la inserción
+        conn.rollback()
+        print(f"Error al añadir color: {e}")
+
+
+##-------------------------------------------------------------
+def add_category(conn):
+    """
+    Pide por teclado nombre de categoria y devuelve el id autogenerado
+    si la inserción en BD se realiza correctamente
+    """
+
+    category_name = input("Nombre categoria: ")
+    if category_name ==  "":
+        print("Debes introducir un nombre de categoria")
+        return
+
+    sql="""
+            INSERT INTO Categoria(nombre) values(%(n)s) returning id
+        """
+    with conn.cursor() as cur:
+        try:
+            cur.execute(sql, {'n':category_name}) 
+            conn.commit() 
+            id = cur.fetchone()[0]
+            print(f"Categoria Añadido con id: {id}.")
+        except psycopg2.Error as e:
+            print(f"Error al añadir la categoria: {e}")
+            conn.rollback()
+
     
 ## ------------------------------------------------------------
 def menu(conn):
@@ -99,7 +170,12 @@ def menu(conn):
         if tecla == 'q':
             break
         elif tecla == '1':
-            add_product(conn)  
+            add_product(conn)
+        elif tecla == '2':
+            add_color(conn)
+        elif tecla == '5':
+            add_category(conn)
+
 
 
 def main():
